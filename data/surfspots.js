edf26dcf1e693dbb91cbd4714f4241d039b74f4b
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-22 00:17:40 -0700

    Uploaded all Images

---
+++ data/surfspots.js
@@ -0,0 +1,12 @@
+// Seal Beach
+// Bolsa Beach
+// Golden West
+// Huntington Beach
+// Newport 
+// Blackies
+// Salt Creek
+// Doheny
+// San Clemente Pier
+// T-Street
+// Trestles
+// San Onofre
