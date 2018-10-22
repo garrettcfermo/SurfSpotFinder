9543195ac48f9c607c9530c3c2907f8dd0886a80
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-22 09:20:20 -0700

    Updated the SurfSpot Data

--- data/surfspots.js
+++ data/surfspots.js
@@ -10,3 +10,153 @@
 // T-Street
 // Trestles
 // San Onofre
+
+var surfSpotArray = [
+  {
+    id:1,
+    name:'Seal Beach',
+    break: 'Pier Break' ,
+    wave_pace: 2,
+    wave_size: 2,
+    surfing_level: 1,
+    perferred_board: 'Longboard',
+    perferred_foot: 'Goofy',
+    wave_direction: 'Left',
+    img_url: '../images/sealbeach.jpg'
+  },
+  {
+    id:2,
+    name:'Bolsa Chica Beach',
+    break: 'Beach Break' ,
+    wave_pace: 2,
+    wave_size: 3,
+    surfing_level: 2,
+    perferred_board: 'Funboard',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/bolsachicabeach.jpg'
+  },
+  {
+    id:3,
+    name:'Golden West',
+    break: 'Beach Break' ,
+    wave_pace: 3,
+    wave_size: 3,
+    surfing_level: 3,
+    perferred_board: 'Funboard',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/goldenwestbeach.jpg'
+  },
+  {
+    id:4,
+    name:'Huntington Beach',
+    break: 'Beach Break' ,
+    wave_pace: 3,
+    wave_size: 3,
+    surfing_level: 2,
+    perferred_board: 'Funboard',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/huntingtonbeach.jpg'
+  },
+  {
+    id:5,
+    name:'Newport Beach',
+    break: ' Beach/Point Break' ,
+    wave_pace: 5,
+    wave_size: 3,
+    surfing_level: 4,
+    perferred_board: 'Thruster',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/newportbeach.jpg'
+  },
+  {
+    id:6,
+    name:'Blackies',
+    break: ' Pier Break' ,
+    wave_pace: 1,
+    wave_size: 1,
+    surfing_level: 1,
+    perferred_board: 'Longboard',
+    perferred_foot: 'Goofy',
+    wave_direction: 'Left',
+    img_url: '../images/blackiesbeach.jpg'
+  },
+  {
+    id:7,
+    name:'Salt Creek',
+    break: 'Point Break' ,
+    wave_pace: 3,
+    wave_size: 3,
+    surfing_level: 4,
+    perferred_board: 'Thruster',
+    perferred_foot: 'Goofy',
+    wave_direction: 'Left',
+    img_url: '../images/saltcreekbeach.jpg'
+  },
+  {
+    id:8,
+    name:'Doheny Beach',
+    break: 'Point Break' ,
+    wave_pace: 2,
+    wave_size: 1,
+    surfing_level: 1,
+    perferred_board: 'Longboard',
+    perferred_foot: 'Regular',
+    wave_direction: 'Right',
+    img_url: '../images/dohenybeach.jpg'
+  },
+  {
+    id:9,
+    name:'San Clemente Beach',
+    break: 'Pier Break' ,
+    wave_pace: 4,
+    wave_size: 2,
+    surfing_level: 3,
+    perferred_board: 'Quad',
+    perferred_foot: 'Left',
+    wave_direction: 'Left',
+    img_url: '../images/sanclementebeach.jpg'
+  },
+  {
+    id:10,
+    name:'T-Street',
+    break: 'Beach Break' ,
+    wave_pace: 3,
+    wave_size: 3,
+    surfing_level: 4,
+    perferred_board: 'Thruster',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/tstreetbeach.jpg'
+  },
+  {
+    id:11,
+    name:'Trestles',
+    break: 'Point Break' ,
+    wave_pace: 4,
+    wave_size: 4,
+    surfing_level: 5,
+    perferred_board: 'Thruster',
+    perferred_foot: 'Right',
+    wave_direction: 'Right',
+    img_url: '../images/trestlesbeach.jpg'
+  },
+  {
+    id:12,
+    name:'San Onofre',
+    break: 'Beach Break' ,
+    wave_pace: 1,
+    wave_size: 1,
+    surfing_level: 1,
+    perferred_board: 'LongBoard',
+    perferred_foot: 'Both',
+    wave_direction: 'Both',
+    img_url: '../images/sanonofrebeach.jpg'
+  }
+]
+
+
+module.exports = surfSpotArray
