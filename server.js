7d2fa8a93424b68be68ede5c2687e46e7988146a
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-23 17:26:55 -0700

    should fix heroku issue

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyparser = require('body-parser')
+const PORT = process.env.PORT || 3000;
 
 const fs = require('fs');
 
@@ -13,7 +14,7 @@ app.use(express.json());
 
 require("./routes/apiroutes")(app);
 
-app.listen(3000, () => console.log('http://localhost:3000'))
+app.listen(PORT, () => console.log('http://localhost:3000'))
 
 
 
